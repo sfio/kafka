565ee01702fc1d4b0744a20de59631d3f8c25f13
krant <aleksey.vasilenko@gmail.com>
2018-10-15 14:03:11 +0300

    Add SASL_PLAINTEXT configuration

--- entry.sh
+++ entry.sh
@@ -1,45 +1,92 @@
 #!/usr/bin/env sh
 set -e
 
-## defaults
-LISTENER=kafka
+## Parse args
 PORT=9092
+LISTENER=kafka
+for i in $@; do case $i in
+	-port=*) PORT="${i#-port=}"; ;;
+	-listener=*) LISTENER="${i#-listener=}"; ;;
+	-principal=*) PRINCIPAL="${i#-principal=}"; ;;
+	-keytab=*) KEYTAB="${i#-keytab=}"; ;;
+	-kdc=*) KDC="${i#-kdc=}"; ;;
+	-silent) SILENT=1; ;;
+esac; done
 
-## parse params
-for i in $@; do
-	case $i in -listener=*)
-		LISTENER="${i#-listener=}"
-	esac
-	case $i in -port=*)
-		PORT="${i#-port=}"
-	esac
-	case $i in -silent)
-		SILENT=1
-	esac
-done
-
+## Generate configs
 KAFKA_PROPS=/kafka/config/server.properties
 echo >> $KAFKA_PROPS
-
-## set connection properties
-echo "listeners=PLAINTEXT://:$PORT" >> $KAFKA_PROPS
-echo "advertised.listeners=PLAINTEXT://$LISTENER:$PORT" >> $KAFKA_PROPS
-
-## speedup shutdown
-echo "controlled.shutdown.enable=false" >> $KAFKA_PROPS
-
-## decrease verbosity
-if [ "$SILENT" ]; then
-	LOG4J_PROPS=/kafka/config/log4j.properties
-	echo "log4j.rootLogger=WARN, stdout" >> $LOG4J_PROPS
-	echo "log4j.logger.kafka=WARN" >> $LOG4J_PROPS
-	echo "log4j.logger.org.apache.kafka=WARN" >> $LOG4J_PROPS
-	echo "log4j.logger.org.apache.zookeeper=WARN" >> $LOG4J_PROPS
-	echo "log4j.logger.org.I0Itec.zkclient.ZkClient=WARN" >> $LOG4J_PROPS
+## SASL_PLAINTEXT security
+if [ "$PRINCIPAL" ] && [ "$KEYTAB" ] && [ "$KDC" ]; then
+	## Extract realm and name form PRINCIPAL
+	PRINCIPAL_REALM=${PRINCIPAL#*@}
+	PRINCIPAL_NAME=${PRINCIPAL%/*}
+	## Inject path to JAAS into KAFKA_OPTS
+	KAFKA_JAAS=/kafka/config/kafka.jaas
+	sed -i '16iKAFKA_OPTS="-Djava.security.auth.login.config=/kafka/config/kafka.jaas"' /kafka/bin/kafka-run-class.sh
+	## Generate JAAS
+	cat >$KAFKA_JAAS <<EOF
+KafkaServer {
+  com.sun.security.auth.module.Krb5LoginModule required
+  useKeyTab=true
+  storeKey=true
+  useTicketCache=true
+  keyTab="$KEYTAB"
+  principal="$PRINCIPAL";
+};
+KafkaClient {
+  com.sun.security.auth.module.Krb5LoginModule required
+  useKeyTab=true
+  keyTab="$KEYTAB"
+  principal="$PRINCIPAL";
+};
+EOF
+	## Generate krb5.conf
+	cat >/etc/krb5.conf <<EOF
+[libdefaults]
+default_realm = $PRINCIPAL_REALM
+rdns = false
+[realms]
+$PRINCIPAL_REALM = {
+  kdc = $KDC
+  rdns = false
+}
+[domain_realm]
+realm = $PRINCIPAL_REALM
+[logging]
+kdc = CONSOLE
+EOF
+	## Kafka server
+	cat >>$KAFKA_PROPS <<EOF
+listeners=SASL_PLAINTEXT://:$PORT
+advertised.listeners=SASL_PLAINTEXT://$LISTENER:$PORT
+inter.broker.listener.name=SASL_PLAINTEXT
+sasl.kerberos.service.name=$PRINCIPAL_NAME
+allow.everyone.if.no.acl.found=true
+controlled.shutdown.enable=false
+EOF
+## PLAINTEXT security
+else
+	cat >>$KAFKA_PROPS <<EOF
+listeners=PLAINTEXT://:$PORT
+advertised.listeners=PLAINTEXT://$LISTENER:$PORT
+controlled.shutdown.enable=false
+EOF
 fi
 
-## start Zookepeer
+## Decrease verbosity
+if [ "$SILENT" ]; then
+	cat >>/kafka/config/log4j.properties <<EOF
+log4j.rootLogger=WARN, stdout
+log4j.logger.kafka=WARN
+log4j.logger.org.apache.kafka=WARN
+log4j.logger.org.apache.zookeeper=WARN
+log4j.logger.org.I0Itec.zkclient.ZkClient=WARN
+EOF
+fi
+
+## Start Zookepeer
 zookeeper-server-start.sh -daemon /kafka/config/zookeeper.properties
 
-## start Kafka
+## Start Kafka
 exec kafka-server-start.sh $KAFKA_PROPS
